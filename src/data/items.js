20bc3d18e58428e670fae6e17269ff32232c8309
khushi2301 <garkhushi12@gmail.com>
2025-04-16 01:06:19 +0530

    Update items.js

--- src/data/items.js
+++ src/data/items.js
@@ -5,28 +5,38 @@ const itemsData = [
         { id: 1, name: "Chips", price: 20, image: "/images/chips.jpg" },
         { id: 2, name: "Cookies", price: 25, image: "/images/cookies.jpg" },
         { id: 3, name: "Chocolate", price: 30, image: "/images/chocolates.jpg" },
-        { id: 7, name: "Nuts", price: 40, image: "/images/nuts.jpg" },
+        { id: 4, name: "Nuts", price: 40, image: "/images/nuts.jpg" },
         
       ],
     },
     {
       name: "Drinks",
       items: [
-        { id: 4, name: "Soda", price: 35, image: "/images/soda.png" },
-        { id: 5, name: "Juice", price: 40, image: "/images/juice.png" },
-        { id: 6, name: "Water", price: 15, image: "/images/water.png" },
-        { id: 11, name: "Soda", price: 35, image: "/images/soda.png" },
-        { id: 12, name: "Juice", price: 40, image: "/images/juice.png" },
-        { id: 13, name: "Water", price: 15, image: "/images/water.png" },
+        { id: 5, name: "Soda", price: 35, image: "/images/soda.png" },
+        { id: 6, name: "Energy drink", price: 40, image: "/images/juice.png" },
+        { id: 7, name: "Water", price: 15, image: "/images/water.png" },
+        { id: 8, name: "cold drink", price: 35, image: "/images/soda.png" },
+        { id: 9, name: "Juice", price: 40, image: "/images/juice.png" },
+        { id: 10, name: "Hot chocolate", price: 15, image: "/images/water.png" },
       ],
     },
      {
       name: "Packaged Meals",
       items: [
-        { id: 4, name: "Cup Noodles", price: 35, image: "/images/cup.png" },
-        { id: 5, name: "Wraps", price: 40, image: "/images/juice.png" },
-        { id: 6, name: "Sandwiches", price: 15, image: "/images/water.png" },
-        { id: 11, name: "Oatmeal Cup", price: 35, image: "/images/soda.png" },
+        { id: 11, name: "Cup Noodles", price: 35, image: "/images/cup.png" },
+        { id: 12, name: "Wraps", price: 40, image: "/images/juice.png" },
+        { id: 13, name: "Sandwiches", price: 15, image: "/images/water.png" },
+        { id: 14, name: "Oatmeal Cup", price: 35, image: "/images/soda.png" },
+       
+      ],
+    },
+    {
+      name: "Dairy Products",
+      items: [
+        { id: 15, name: "Yogurt", price: 35, image: "/images/cup.png" },
+        { id: 16, name: "Flavoured milk", price: 40, image: "/images/juice.png" },
+        { id: 17, name: "Cheese", price: 15, image: "/images/water.png" },
+        { id: 18, name: "Oatmeal Cup", price: 35, image: "/images/soda.png" },
        
       ],
     },
