be650ffdcb0f5e9bf55583892bf5a0541794e1a5
khushi2301 <garkhushi12@gmail.com>
2025-04-15 15:49:03 +0530

    Update items.js

--- src/data/items.js
+++ src/data/items.js
@@ -20,6 +20,16 @@ const itemsData = [
         { id: 13, name: "Water", price: 15, image: "/images/water.png" },
       ],
     },
+     {
+      name: "Packaged Meals",
+      items: [
+        { id: 4, name: "Cup Noodles", price: 35, image: "/images/soda.png" },
+        { id: 5, name: "Wraps", price: 40, image: "/images/juice.png" },
+        { id: 6, name: "Sandwiches", price: 15, image: "/images/water.png" },
+        { id: 11, name: "Oatmeal Cup", price: 35, image: "/images/soda.png" },
+       
+      ],
+    },
     
   ];
   
